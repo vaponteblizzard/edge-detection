bff0bdd578304e586b63b5b256bfc4ffb461f539
Victoria <vaponteblizzard@gmail.com>
2016-09-27 04:11:57 -0500

    adds pretty printing and local video device edge detection

---
+++ detect-edges-matplot.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('bear.jpg',0)
+edges = cv2.Canny(img,200,200)
+
+plt.subplot(121), plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(edges,cmap = 'gray')
+plt.title('Edged'), plt.xticks([]), plt.yticks([])
+
+plt.show()
+
--- detect-edges.py
+++ detect-edges.py
@@ -1,14 +1,12 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 
-img = cv2.imread('bear.jpg',0)
-edges = cv2.Canny(img,200,200)
+img = cv2.imread('bear.jpg')
+print(img)
+edges = cv2.Canny(img,100,200)
 
-plt.subplot(121),plt.imshow(img,cmap = 'white')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'white')
-plt.title('Edged'), plt.xticks([]), plt.yticks([])
+print (img)
 
-plt.show()
+cv2.imshow('bear',edges)
 
+cv2.waitKey(0)
